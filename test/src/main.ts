92106a9079896afe29a475f5f7e35d8ef4adeeee
PrabinGautam <prabeen122@gmail.com>
2023-11-22 14:33:45 +0545

    feat: added test file

---
+++ test/src/main.ts
@@ -0,0 +1,15 @@
+import "./style.css";
+import typescriptLogo from "./typescript.svg";
+// import { getResponse } from "./response.ts";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>next-nfetch</h1>
+    <p class="read-the-docs">
+      Check response file about the usage
+    </p>
+  </div>
+`;
