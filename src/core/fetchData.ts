f80e0defa87b2ac5e17d4726e5447d33f1672cbf
PrabinGautam <prabeen122@gmail.com>
2023-11-27 20:35:40 +0545

    fix: fixed patch method being lowercase

--- src/core/fetchData.ts
+++ src/core/fetchData.ts
@@ -2,9 +2,11 @@ import handleResponse from "../helper/handleResponse";
 
 // @ts-ignore
 async function fetchData(endpoint, requestOptions, controller) {
+  const isPatch = requestOptions.method === "patch";
   try {
     const response = await fetch(endpoint, {
       ...requestOptions,
+      method: isPatch ? "PATCH" : requestOptions.method,
       signal: controller.signal,
     });
 
